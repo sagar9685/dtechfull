092b7b87d191c084efe7225e0b784f109d08373f
sagar9685 <sagargupta12396@gmail.com>
2024-12-03 16:04:09 +0530

    correct

--- deetech-backend/index.js
+++ deetech-backend/index.js
@@ -14,12 +14,7 @@ const PORT = process.env.PORT || 3000; // Use PORT from .env if available
 
 // Middlewares
 app.use(express.json());
-const corsOptions = {
-  origin: process.env.APP_URI, // Ensure this matches the frontend URL
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Add any other methods as needed
-  credentials: true, // Allow credentials (cookies, etc.)
-};
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(morgan("dev"));
 
 app.use("/api/auth", require("./routes/authroutes"));
